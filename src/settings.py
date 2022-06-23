7500702ae2f0bc51749b361ddc715eb9651b017c
josep sitjar <josepsitjar@gmail.com>
2022-06-23 08:13:18 +0200

    djoser

--- src/settings.py
+++ src/settings.py
@@ -29,14 +29,22 @@ ALLOWED_HOSTS = ['127.0.0.1']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        #'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAdminUser',
     )
 }
 
 CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:8000', 'http://localhost:8080']
+CORS_ORIGIN_ALLOW_ALL=True
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',
+    'http://127.0.0.1:8000',
+)
 
 # Application definition
 
